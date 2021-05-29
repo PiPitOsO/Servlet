e62cd7ab2fc6f3053d8016159b9e9b7cb6f77559
PiPitOsO <vladdiablo1@mail.ru>
2021-05-29 23:01:20 +0300

    id

--- src/main/java/repository/PostRepository.java
+++ src/main/java/repository/PostRepository.java
@@ -8,7 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 // Stub
 public class PostRepository {
-    private AtomicInteger id = new AtomicInteger(1);
+    private AtomicInteger id = new AtomicInteger(0);
     private Map<Integer, Post> map = new ConcurrentHashMap<>();
 
     public List<Post> all() {
@@ -19,9 +19,22 @@ public class PostRepository {
         return Optional.ofNullable(map.get(id));
     }
 
+//    public Post save(Post post) {
+//        int newId = id.incrementAndGet();
+//        map.put(newId, post);
+//        return post;
+//    }
+
     public Post save(Post post) {
-        int newId = id.incrementAndGet();
-        map.put(newId, post);
+        int idd = (int) post.getId();
+        if (idd == 0) {
+            // добавим новый
+            int newId = id.incrementAndGet();
+            map.put(newId, post);
+        } else {
+            // заменим старый
+            map.put(idd, post);
+        }
         return post;
     }
 
